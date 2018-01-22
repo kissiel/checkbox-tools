ec5b60d21bd370978e14226ae277d51f9652f5ff
kissiel <kissiel@gmail.com>
2018-01-22 23:18:20 +0100

    super-crude version of tri-check and print_sequence
    Signed-off-by: kissiel <kissiel@gmail.com>

---
+++ print_sequence.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+from plainbox.abc import IJobQualifier
+from plainbox.impl.session.assistant import SessionAssistant
+from plainbox.impl.unit.unit_with_id import UnitWithId
+from plainbox.impl.unit.job import JobDefinition
+from plainbox.impl.unit.template import TemplateUnit
+
+sa = SessionAssistant('print_sequence')
+
+units = dict()
+template_units = dict()
+
+def template_to_re(uid):
+    #if 'com.canonical.certification::input/clicking' in uid:
+    #    import pdb; pdb.set_trace()
+    uid = uid.replace('{__index__}', r'\d+')
+    greedy = re.sub(r'\{.+\}', '.+', uid)
+    nongreedy = re.sub(r'\{.+?\}', '.+?', uid)
+    return nongreedy, greedy
+
+
+def generate_unit_map():
+    for unit in sa._context._unit_list:
+        u_type = type(unit)
+        if u_type == TemplateUnit:
+            unit.re_matcher, unit.re_greedy = template_to_re(unit.id)
+            template_units[unit.id] = unit
+        if not (issubclass(u_type, JobDefinition)):
+            # print('skipping {}'.format(type(unit)))
+            continue
+        units[unit.id] = unit
+    
+
+
+def get_run_sequence(tp_unit, include_nested = True):
+    result = []
+    if include_nested:
+        for tp in tp_unit.get_nested_part():
+            # print("NESTED {}".format(tp))
+            result += get_run_sequence(sa.get_test_plan(tp.id))
+    for line in tp_unit.include.split('\n'):
+        if not line:
+            continue
+            
+        if line.startswith('#'):
+            continue
+        line = line.split()[0]
+        kind = 'unknown'
+        extras = ''
+        full_id = line
+        if "::" not in full_id:
+            qid = tp_unit.qualify_id(line)
+            full_id = qid
+        if '::after-suspend' in full_id:
+            full_id = full_id.replace('::after-suspend-', '::')
+            extras = '  <= {} REMOVED "after-suspend-" PREFIX'.format(full_id)
+                
+        if full_id in units.keys():
+            plugin = units[full_id].plugin
+            if plugin in ['shell', 'resource', 'attachment']:
+                kind = 'automatic'
+            else:
+                kind = 'manual'
+        if kind == 'unknown':
+            candidates = []
+            for tid, tunit in template_units.items():
+                matches = re.match(tunit.re_matcher, full_id)
+                if matches:
+                    candidates.append(tunit)
+            if not candidates:
+                for tid, tunit in template_units.items():
+                    matches = re.match(tunit.re_greedy, full_id)
+                    if matches:
+                        candidates.append(tunit)
+
+            tunit = None
+            if len(candidates) == 1:
+                tunit = candidates[0]
+            elif len(candidates) > 1:
+                from difflib import SequenceMatcher
+                tid = sorted([c.id for c in candidates], key=lambda a: SequenceMatcher(
+                    None, a, line).ratio(), reverse=True)[0]
+                tunit = template_units[tid]
+
+            if tunit:
+                if tunit.get_record_value('plugin') in ['shell', 'resource', 'attachment']:
+                    kind = 'automatic'
+                else:
+                    kind = 'manual'
+                extras = '   <= {}'.format(tunit.id)
+
+
+        result.append((full_id, kind, extras))
+    return result
+
+    
+
+def get_jobs_from_tp(tp_unit):
+    selected = []
+    quals = tp_unit.get_qualifier()
+    for uid in units.keys():
+        if quals.get_vote(units[uid]) == IJobQualifier.VOTE_INCLUDE:
+            selected.append(uid)
+    # also recursively visit nested test plans
+    return selected
+
+def init():
+    generate_unit_map()
+
+
+
+def main():
+
+    tp = None
+    if len(sys.argv) != 2:
+        # raise SystemExit("Missing argument: test plan!")
+        tp = 'com.canonical.ce::stella-full-16-04'
+    else:
+        tp = sys.argv[1]
+
+    sa.select_providers('*')
+    sa.start_new_session('print_sequence')
+    all_tps = sa.get_test_plans()
+
+    if tp not in all_tps:
+        raise SystemExit('"{}" test plan not found!'.format(tp))
+
+
+    init()
+    tp_unit = sa.get_test_plan(tp)
+
+    def list_tp_sets():
+        fulls = [tp for tp in all_tps if tp.endswith('full')]
+        for tp in fulls:
+            basename = tp[:-4]
+            others = [tp for tp in all_tps if tp.startswith(basename)]
+            print(tp)
+            for t in others:
+                if t == tp: continue
+                print(t)
+            print('\n\n')
+        print(fulls)
+
+
+    new_units = dict()
+    global units
+    for uid in units.keys():
+        if '{__index__}' in uid:
+            for i in range(10):
+                data = units[uid]._data.copy()
+                data['id'] = uid.replace('{__index__}', str(i))
+                new_units[data['id']] = TemplateUnit(data)
+        else:
+            new_units[uid] = units[uid]
+    units = new_units
+
+
+
+
+    #quals = tp_unit.get_qualifier()
+    #for i in get_jobs_from_tp(tp_unit):
+    #    print(i)
+    #print(get_jobs_from_tp(tp_unit))
+    #print(len(get_jobs_from_tp(tp_unit)))
+
+    #import pdb; pdb.set_trace()
+    for uid, kind, extras in get_run_sequence(tp_unit):
+        print('{0:10}{1}{2: >130}'.format(kind, uid, extras).strip())
+
+
+if __name__ == '__main__':
+    main()
---
+++ tri-check.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+import os
+import re
+import shutil
+import sys
+from plainbox.abc import IJobQualifier
+from plainbox.impl.session.assistant import SessionAssistant
+from plainbox.impl.unit.unit_with_id import UnitWithId
+from plainbox.impl.unit.job import JobDefinition
+from plainbox.impl.unit.template import TemplateUnit
+from plainbox.impl.unit.testplan import TestPlanUnit
+
+from print_sequence import init, get_run_sequence
+import print_sequence
+
+sa = print_sequence.sa 
+
+def unqualify_id(tp_unit, uid):
+    namespace = tp_unit.qualify_id('')
+    if uid.startswith(namespace):
+        return  uid.replace(namespace, '')
+
+
+def print_missing(all_tps):
+    endings = ['full', 'manual', 'automated']
+    for ending in endings:
+        to_check = endings.copy()
+        to_check.remove(ending)
+        for tp in all_tps:
+            missing = []
+            if tp.endswith(ending):
+                base = tp[:-len(ending)]
+                for e in to_check:
+                    if base+e not in all_tps:
+                        missing.append(base+e)
+            if missing:
+                print("+++ {}".format(tp))
+                for m in missing:
+                    print("  + MISSING: {}".format(m))
+
+def write_tp_unit(tpu):
+    write_order = ['id', 'unit', '_name', '_description', 'include',
+            'bootstrap_include', 'mandatory_include', 'exclude',
+            'nested_part', 'estimated_duration']
+    multiline_fields = ['_description', 'include', 'bootstrap_include',
+                        'mandatory_include', 'exclude', 'nested_part']
+
+    tp_record = ''
+    for field in write_order:
+        if field.startswith('_'):
+            val = tpu.__getattribute__(field[1:])
+        else:
+            val = tpu.__getattribute__(field)
+        if not val and field != 'include':
+            continue
+        tp_record += field + ':'
+        if field in multiline_fields:
+            tp_record += '\n'
+            for line in val.split('\n'):
+                if line:
+                    tp_record += ' ' + line + '\n'
+        else:
+            tp_record += ' ' + val + '\n'
+
+    for symbol in tpu.fields.get_all_symbols():
+        field = symbol.name
+        if field not in write_order and '_' + field not in write_order:
+            pass
+            # print('extra field: {}'.format(field))
+    return tp_record
+
+def check_tp(tpid, all_tps):
+    tp_unit = sa.get_test_plan(tpid)
+    seq = get_run_sequence(tp_unit, True)
+    manuals = [uid for uid, kind, extras in seq if kind == 'manual']
+    autos = [uid for uid, kind, extras in seq if kind == 'automatic']
+    unknowns = [uid for uid, kind, extras in seq if kind == 'unknown']
+    manual_tp = tpid[:-4] + 'manual' 
+    new_man_pxu = ""
+    new_auto_pxu = ""
+    
+    if manual_tp not in all_tps:
+        print("missing {}".format(manual_tp))
+        new_man_tp = TestPlanUnit(tp_unit._raw_data)
+        # remove the namespace prefix
+        unqualified_ids = []
+        for uid in manuals:
+            namespace = tp_unit.qualify_id('')
+            if uid.startswith(namespace):
+                uid = uid.replace(namespace, '')
+            unqualified_ids.append(uid)
+        new_man_tp.id = unqualify_id(tp_unit, manual_tp)
+        new_man_tp.include = "\n".join(unqualified_ids)
+        new_man_tp.name += ' (Manual)'
+        new_man_tp.description += ' (Manual)'
+        new_man_pxu = write_tp_unit(new_man_tp)
+    else:
+        print("{} already there")
+        man_seq = get_run_sequence(sa.get_test_plan(manual_tp))
+        man_seq_ids = [uid for uid, kind, extras in man_seq]
+        if man_seq_ids != manuals:
+            print("BUT HAS WRONG INCLUDE")
+            print("FULL:\n{}\n\n VS \n\nMANUAL:\n{}".format(
+                "\n".join(manuals), "\n".join(man_seq_ids)))
+    auto_tp = tpid[:-4] + 'automated' 
+    if auto_tp not in all_tps:
+        # print("missing {}".format(auto_tp))
+        new_auto_tp = TestPlanUnit(tp_unit._raw_data)
+        # remove the namespace prefix
+        unqualified_ids = []
+        for uid in autos:
+            unqualified_ids.append(unqualify_id(tp_unit, uid))
+        new_auto_tp.id = unqualify_id(tp_unit, auto_tp)
+        new_auto_tp.include = "\n".join(unqualified_ids)
+        new_auto_tp.name += ' (Automated)'
+        new_auto_tp.description += ' (Automated)'
+        new_auto_pxu = write_tp_unit(new_auto_tp)
+    else:
+        print("{} already there")
+        auto_seq = get_run_sequence(sa.get_test_plan(auto_tp))
+        auto_seq_ids = [uid for uid, kind, extras in auto_seq]
+        if auto_seq_ids != autos:
+            print("BUT HAS WRONG INCLUDE")
+            print("FULL:\n{}\n\n VS \n\AUTOMATED:\n{}".format(
+                "\n".join(autos), "\n".join(auto_seq_ids)))
+    if not new_man_pxu and not new_auto_pxu:
+        return
+    tp_unit.include = ""
+    tp_unit.id = unqualify_id(tp_unit, tp_unit.id)
+    tp_unit.nested_part = "\n".join([manual_tp, auto_tp])
+    new_full_pxu = write_tp_unit(tp_unit)
+    with open(tp_unit.origin.source.filename, 'rt') as f:
+        pxu = f.readlines()
+    backup_path = tp_unit.origin.source.filename + '.bkp'
+    if os.path.exists(backup_path):
+        print("Backup file already present. Not overwriting: {}".format(
+            backup_path))
+    else:
+        shutil.copyfile(tp_unit.origin.source.filename, backup_path)
+        print("Backup made: {}".format(backup_path))
+
+    new_pxu = pxu[:tp_unit.origin.line_start-1]
+    new_pxu.append(new_full_pxu)
+    new_pxu.append("\n")
+    new_pxu.append(new_man_pxu)
+    new_pxu.append("\n")
+    new_pxu.append(new_auto_pxu)
+    new_pxu.append("\n")
+    new_pxu += pxu[tp_unit.origin.line_end-1:]
+    with open(tp_unit.origin.source.filename, 'wt') as f:
+        f.write("".join(new_pxu))
+    print("{} rewritten!".format(tp_unit.origin.source.filename))
+
+    if False:
+        print("MANUAL:")
+        for i in manuals: print(i)
+        print("AUTOMATIC:")
+        for i in autos: print(i)
+        print("UNKNOWNS:")
+        for i in unknowns: print(i)
+
+
+
+def main():
+    sa.select_providers('*')
+    sa.start_new_session('tri_check')
+    all_tps = sa.get_test_plans()
+    init()
+    if len(sys.argv) > 1:
+        all_tps = sys.argv[1:]
+
+    for tp in all_tps:
+        if tp.endswith('full'):
+            # print('analyzing {}'.format(tp))
+            tp_unit = sa.get_test_plan(tp)
+            if not tp_unit.nested_part:
+                check_tp(tp, all_tps)
+                #print(tp)
+            #write_tp_unit(tp_unit)
+            #seq = get_run_sequence(tp_unit, False)
+            #if seq:
+            #    print("{} explicitly includes units".format(tp))
+            #for uid, kind, extras in seq:
+            #    print('{0:10}{1}{2: >130}'.format(kind, uid, extras).strip())
+
+if __name__ == '__main__':
+    main()
