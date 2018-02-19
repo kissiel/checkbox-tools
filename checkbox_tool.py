d4d5156cf2bcf403badb004795c7dd366834f39a
kissiel <kissiel@gmail.com>
2018-02-19 16:11:52 +0100

    add annotate TP funcionality

--- checkbox_tool.py
+++ checkbox_tool.py
@@ -76,6 +76,73 @@ class CheckboxTool:
                 continue
             self.units[unit.id] = unit
 
+    def get_unit_info(self, line, qualifier_unit):
+        kind = 'unknown'
+        extras = ''
+        full_id = line
+        ret_plugin = None
+        if "::" not in full_id:
+            qid = qualifier_unit.qualify_id(full_id)
+            full_id = qid
+        if '::after-suspend' in full_id:
+            full_id = full_id.replace('::after-suspend-', '::')
+            extras = '  <= {} REMOVED "after-suspend-" PREFIX'.format(full_id)
+        if full_id in self.units.keys():
+            plugin = self.units[full_id].plugin
+            ret_plugin = plugin
+            if plugin in ['shell', 'resource', 'attachment']:
+                kind = 'automatic'
+            else:
+                kind = 'manual'
+            if self.units[full_id].depends:
+                for dep_id in self.units[full_id].depends.split():
+                    dep_kind, _ = self.get_kind_for_unit(dep_id, self.units[full_id])
+                    if dep_kind == 'manual':
+                        extras += 'deps on manual'
+        if kind == 'unknown':
+            candidates = []
+            for tid, tunit in self.template_units.items():
+                matches = re.match(tunit.re_matcher, full_id)
+                if matches:
+                    candidates.append(tunit)
+            if not candidates:
+                for tid, tunit in self.template_units.items():
+                    matches = re.match(tunit.re_greedy, full_id)
+                    if matches:
+                        candidates.append(tunit)
+            if not candidates:
+                # ordinary matching: from include regex to template_ids
+                for tid, tunit in self.template_units.items():
+                    matches = re.match(full_id, tid)
+                    if matches:
+                        candidates.append(tunit)
+
+            tunit = None
+            if len(candidates) == 1:
+                tunit = candidates[0]
+            elif len(candidates) > 1:
+                from difflib import SequenceMatcher
+                tid = sorted([c.id for c in candidates], key=lambda a: SequenceMatcher(
+                    None, a, line).ratio(), reverse=True)[0]
+                tunit = self.template_units[tid]
+
+            if tunit:
+                ret_plugin = tunit.get_record_value('plugin')
+                if tunit.get_record_value('plugin') in ['shell', 'resource', 'attachment']:
+                    kind = 'automatic'
+                else:
+                    kind = 'manual'
+                if tunit.get_record_value('depends'):
+                    for dep_id in tunit.get_record_value('depends').split():
+                        dep_kind, _ = self.get_kind_for_unit(dep_id, tunit)
+                        if dep_kind == 'manual':
+                            extras += 'deps on man'
+                            kind = 'manual'
+        if not ret_plugin:
+            ret_plugin = 'UNKNOWN'
+        return ret_plugin, extras
+
+
     def get_kind_for_unit(self, line, qualifier_unit):
         kind = 'unknown'
         extras = ''
@@ -260,3 +327,25 @@ class CheckboxTool:
         with open(tp_unit.origin.source.filename, 'wt') as f:
             f.write("".join(new_pxu))
         print("{} rewritten!".format(tp_unit.origin.source.filename))
+
+    def annotated_tp(self, tp_id):
+        tp_unit = self.sa.get_test_plan(tp_id)
+        new_include = ''
+        for line in tp_unit.include.split('\n'):
+            if not line:
+                new_include += line + '\n'
+                continue
+            if line.startswith('#'):
+                new_include += line + '\n'
+                continue
+            if '#!-' in line:
+                new_include += line + '\n'
+                continue
+            sections = line.split()
+            annotations = line[len(sections[0]):]
+            pattern = sections[0]
+            pattern = pattern.split()[0]
+            plugin, extras = self.get_unit_info(pattern, tp_unit)
+            new_include += '{}    #!- {} - {}\n'.format(line, plugin, extras)
+        tp_unit.include = new_include
+        return generate_tp_unit(tp_unit)
