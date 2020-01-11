5196dfa3e3ac2791763909ff6765803f1a54f0bd
kissiel <kissiel@gmail.com>
2020-01-11 12:56:48 +0100

    scaling kinda done

--- scaling.py
+++ scaling.py
@@ -1,10 +1,12 @@
 import collections
 import concurrent
 import datetime
+import json
 import multiprocessing
 import os
 import shutil
 import subprocess
+import statistics
 import sys
 import tempfile
 import time
@@ -24,6 +26,7 @@ class TaskPool:
         for commit, task in self._task_map.items():
             if task.exception():
                 print("Task failed for commit {}. {}".format(commit, task.exception()))
+        return [r.result() for r in self._task_map.values()]
 
 class BenchmarkingTask:
     def __init__(self, path, commit):
@@ -43,15 +46,15 @@ class BenchmarkingTask:
             )
             shutil.copy(os.path.join(base_dir, 'benchmark.py'), os.path.join(tmp, 'benchmark.py'))
 
-            try:
-                os.chdir(tmp)
-                for scenario in ['small', 'templatey', 'bootstrap-only']:
-                    out = subprocess.check_output(['python3', 'benchmark.py', scenario])
-                    elapsed = float(out.splitlines()[-1])
-                    print('{} - {} : {}'.format(self._commit, scenario, elapsed))
-            finally:
-                os.chdir(base_dir)
-            return
+            # for scenario in ['small', 'templatey', 'bootstrap-only']:
+            results = []
+            for scenario in ['small', 'templatey', 'bootstrap-only']:
+                out = subprocess.check_output(
+                    ['python3', os.path.join(tmp, 'benchmark.py'), scenario])
+                elapsed = float(out.splitlines()[-1])
+                print('{} - {} : {}'.format(self._commit, scenario, elapsed))
+                results.append((self._commit, scenario, elapsed))
+            return  results
 
             commit_timestamp = subprocess.check_output('git show -s --format=%ct', shell=True).decode(sys.stdout.encoding)
             commit_dt = datetime.datetime.fromtimestamp(int(commit_timestamp))
@@ -67,7 +70,8 @@ class BenchmarkingTask:
             if not os.path.exists(score_path):
                 print('{} info not found, benchmarking'.format(commit))
                 scores = dict()
-                for scenario in ['small', 'templatey', 'bootstrap-only']:
+                # for scenario in ['small', 'templatey', 'bootstrap-only']:
+                for scenario in ['small', 'bootstrap-only']:
                     scores[scenario] = bench(scenario)
                     #scores[scenario] = 2.0
                 measurement['scores'] = scores
@@ -103,13 +107,34 @@ class GitRepo:
 def main():
     master_path = sys.argv[1]
     repo = GitRepo(master_path)
-    commits = repo.get_merge_commits()
-    pool = TaskPool(1)
-    for commit in commits[:10]:
-        pool.add_task(BenchmarkingTask(master_path, commit))
-    pool.run_and_wait()
-
-
+    merge_commits = repo.get_merge_commits()
+    LARGEST_POOL = 48 # 48
+    global_res = dict()
+    for n in range(1, LARGEST_POOL + 1):
+        print("*"*20, "TASK POOL SIZE: {}".format(n), "*"*20)
+        pool = TaskPool(n)
+        for commit in merge_commits[:1]:
+            for i in range(n * 3):
+                pool.add_task(BenchmarkingTask(master_path, commit))
+        results = dict()
+        for task_done in pool.run_and_wait():
+            for commit, scenario, elapsed in task_done:
+                if scenario not in results.keys():
+                    results[scenario] = dict()
+                if scenario not in global_res.keys():
+                    global_res[scenario] = dict()
+                if commit not in results[scenario].keys():
+                    results[scenario][commit] = list()
+                results[scenario][commit].append(elapsed)
+            for scenario, commits in results.items():
+                print("="*20, scenario, "="*20)
+                for commit, times in commits.items():
+                    median = statistics.median(times)
+                    print("{} : {}".format(commit, median))
+                global_res[scenario][n] = median
+                #print(global_res)
+    js = json.dumps(global_res, indent="  ")
+    print(js)
 
 if __name__ == '__main__':
     main()
