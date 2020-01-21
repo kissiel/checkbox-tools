2e1fa7e3c2b0eccc6a6f0fb0e4a06075c8c91adc
kissiel <kissiel@gmail.com>
2020-01-21 21:32:04 +0100

    rename provider

--- benchmark.py
+++ benchmark.py
@@ -24,25 +24,34 @@ import subprocess
 import sys
 import time
 
+from pprint import pprint
+
 """
 Benchmark Checkbox with different scenarios.
 
-This program runs a particular launcher and measures how long it took to run it.
-Place this file and the 2019.com.canonical.certification:metabench provider in the checkbox-ng tree and run it.
+This program runs a particular launcher and measures how long it took to run
+it.  Place this file and the benchmarking-provider provider in the checkbox-ng
+tree and run it.
 """
 
 def prepare_venv():
     shutil.rmtree('venv', ignore_errors=True)
-    subprocess.run("./mk-venv", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    subprocess.run(". venv/bin/activate; ./2019.com.canonical.certification:metabench/manage.py develop -d $PROVIDERPATH", shell=True, stdout=subprocess.DEVNULL)
+    subprocess.run(
+        "./mk-venv", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(
+            (". venv/bin/activate; benchmarking-provider/manage.py"
+            "develop -d $PROVIDERPATH"),
+            shell=True, stdout=subprocess.DEVNULL)
 
 def run_via_remote(scenario):
     # start the slave
     try:
-        slave_proc = subprocess.Popen('. venv/bin/activate; checkbox-cli slave', shell=True, start_new_session=True)
+        slave_proc = subprocess.Popen(
+            '. venv/bin/activate; checkbox-cli slave',
+            shell=True, start_new_session=True)
     except subprocess.CalledProcessError as exc:
         raise SystemExit("Failed to run the slave")
-    launcher = "./2019.com.canonical.certification:metabench/launcher-{}".format(scenario)
+    launcher = "benchmarking-provider/launcher-{}".format(scenario)
     with contextlib.ExitStack() as stack:
         def kill_slave(*args):
             with contextlib.suppress(ProcessLookupError):
@@ -50,20 +59,24 @@ def run_via_remote(scenario):
         stack.push(kill_slave)
         try:
             start = time.time()
-            subprocess.run(". venv/bin/activate; checkbox-cli master localhost {}".format(launcher), shell=True, stderr=subprocess.STDOUT)
+            subprocess.run(
+                ". venv/bin/activate; checkbox-cli master localhost {}".format(
+                    launcher), shell=True, stderr=subprocess.STDOUT)
             stop = time.time()
         except subprocess.CalledProcessError as exc:
             print(exc.stdout.decode(sys.stdout.encoding))
-            raise SystemExit("Failed to remotely run scenario {}".format(scenario))
+            raise SystemExit("Failed to remotely run scenario {}".format(
+                scenario))
         if slave_proc.poll() is not None:
             raise SystemExit("Slave died by its own. Benchmarking failed")
     return stop - start
 
 def run_locally(scenario):
-    launcher = "./2019.com.canonical.certification:metabench/launcher-{}".format(scenario)
+    launcher = "benchmarking-provider/launcher-{}".format(scenario)
     try:
         start = time.time()
-        subprocess.run(". venv/bin/activate; checkbox-cli {}".format(launcher), shell=True, stderr=subprocess.STDOUT)
+        subprocess.run(". venv/bin/activate; checkbox-cli {}".format(
+            launcher), shell=True, stderr=subprocess.STDOUT)
         stop = time.time()
     except subprocess.CalledProcessError as exc:
         print(exc.stdout.decode(sys.stdout.encoding))
@@ -72,15 +85,16 @@ def run_locally(scenario):
 
 def main():
     os.chdir(os.path.split(os.path.abspath(__file__))[0])
-    launchers = glob.glob('2019.com.canonical.certification:metabench/launcher-*')
-    scenarios = [s.replace('2019.com.canonical.certification:metabench/launcher-', '') for s in launchers]
+    launchers = glob.glob('benchmarking-provider/launcher-*')
+    scenarios = [
+        s.replace('benchmarking-provider/launcher-', '') for s in launchers]
     results = dict()
     for scenario in scenarios:
         local_result = run_locally(scenario)
         remote_result = run_via_remote(scenario)
         results['local-{}'.format(scenario)] = local_result
         results['remote-{}'.format(scenario)] = remote_result
-    print(results)
+    pprint(results)
 if __name__ == '__main__':
     main()
 
